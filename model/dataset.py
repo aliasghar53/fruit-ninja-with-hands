9e2137e12998fbc2d86fbac6792a5bb7a909760a
aliasghar53 <aliasghar1994@gmail.com>
2022-08-15 14:13:21 -0400

    added resize

--- model/dataset.py
+++ model/dataset.py
@@ -29,18 +29,18 @@ class EgoHands(Dataset):
                 self.masks.append(mask)
         
         if (mode == "train") and (transform is None):
-            self.transform = T.Compose([
-                                        T.Resize((256,512)),
+            self.transform = T.Compose([                                        
                                         T.ToTensor(),
+                                        T.Resize((256,512)),
                                         T.ColorJitter(hue=0.1, saturation=0.25),
                                         T.RandomHorizontalFlip(0.5),
                                         T.ConvertImageDtype(torch.float),
                                         T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                                         ])
         elif (mode == "eval") and (transform is None):
-            self.transform = T.Compose([
-                                        T.Resize((256,512)),
+            self.transform = T.Compose([                                        
                                         T.ToTensor(),
+                                        T.Resize((256,512)),
                                         T.ConvertImageDtype(torch.float),
                                         T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
                                         ])
